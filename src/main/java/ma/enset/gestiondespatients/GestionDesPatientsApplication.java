13c948d25f07b19e82c2a1dbaeabb8b792de3e9e
widad-banane <widadbanane03@gmail.com>
2025-03-17 09:45:51 +0000

    Initial commit

---
+++ src/main/java/ma/enset/gestiondespatients/GestionDesPatientsApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.gestiondespatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesPatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesPatientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/gestiondespatients/GestionDesPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.gestiondespatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesPatientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
